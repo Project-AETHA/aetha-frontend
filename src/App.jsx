66108658bbb09f89f72c9dfcc1c1b5ea16b0e3e2
NightHawkNB <nipunbathiya1256@outlook.com>
2024-06-21 10:29:11 +0530

    Switched to NEXT UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import "./App.css";
+import Navigation from "./components/Navigation";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-//* Importing components
-import Navbar from "./assets/components/Navbar";
-import Home from "./assets/pages/Home";
-import LoginPage from "./assets/pages/LoginPage";
-import SignupPage from "./assets/pages/SignupPage";
-
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import Reading from "./pages/Reading";
+import ViewAllBooks from "./pages/ViewAllBooks"
+import Novels from "./pages/Novels";
+import Footer from "./components/Footer";
 
 function App() {
   return (
       <Router>
-        <div className="app w-full bg-white h-full">
-          <Navbar />
-          <div className="content">
+        <Navigation />
+
+        <div className="content">
             <Routes>
               <Route exact path="/" element={<div> Landing Page </div>} />
               <Route exact path="/home" element={<Home />} />
+              <Route exact path="/reading" element={<Reading />} />
+              <Route exact path="/novels" element={<Novels />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/signup" element={<SignupPage />} />
             </Routes>
           </div>
-        </div>
+
+          <Footer />
       </Router>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-//? Importing Fonts
-import 'typeface-poppins';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="light">
+        <main className="text-foreground bg-background">
+          <App />
+        </main>
+      </NextThemesProvider>
+    </NextUIProvider>
+  </React.StrictMode>
+);
