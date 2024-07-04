5583dda80487e4dc39716937e3f984a68a97e43c
hansimadhusha <madhushahansini.perera@gmail.com>
2024-07-04 12:47:14 +0530

    poems page ui changes

--- src/pages/Poems.jsx
+++ src/pages/Poems.jsx
@@ -1,6 +1,16 @@
 
 import React from 'react';
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import {
+  Accordion,
+  AccordionItem,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image
+} from "@nextui-org/react";
 import Poem from '../components/Poems/Poem';
 import { Button } from "@nextui-org/react";
 
@@ -46,6 +56,18 @@ const Poems = () => {
         { color: "secondary", text: "Sci-Fi" }
       ],
       followers: formatFollowers(21315)
+    },
+    {
+      id: 3,
+      user: { displayName: "dilmi siriwardhana" },
+      duration: "7hr",
+      title: "MongoDB",
+      content: "empty well peace beautiful sign essential cool according soon song brought grade find threw slowly heavy lady men combination eat depend needs person clock.children cowboy primitive noise four test story lose on price burst captured sell audience sick direction name body damage numeral nine gulf torn potatoes",
+      tags: [
+        { color: "success", text: "Ongoing" },
+        { color: "secondary", text: "Sci-Fi" }
+      ],
+      followers: formatFollowers(78315)
     }
   ]
 
@@ -62,6 +84,7 @@ const Poems = () => {
             className="p-2 flex flex-col gap-1 col-span-2"
             variant="shadow"
             itemClasses={itemClasses}
+  
           >
             <AccordionItem
               key="1"
@@ -72,13 +95,13 @@ const Poems = () => {
             >
 
               <div className=' flex flex-col mx-4'>
-                <Button color="primary" className='my-0.5'>
+                <Button color="primary" variant='light' className='my-0.1'>
                   Secondary
                 </Button>
-                <Button color="primary" className='my-0.5'>
+                <Button color="primary" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
-                <Button color="primary" className='my-0.5'>
+                <Button color="primary" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
               </div>
@@ -90,13 +113,13 @@ const Poems = () => {
               title="Apps Permissions"
             >
               <div className=' flex flex-col mx-4'>
-                <Button color="secondary" className='my-0.5'>
+                <Button color="secondary" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
-                <Button color="secondary" className='my-0.5'>
+                <Button color="secondary" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
-                <Button color="secondary" className='my-0.5'>
+                <Button color="secondary" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
               </div>
@@ -108,13 +131,13 @@ const Poems = () => {
               title="Pending tasks"
             >
               <div className=' flex flex-col mx-4'>
-                <Button color="danger" className='my-0.5'>
+                <Button color="danger" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
-                <Button color="danger" className='my-0.5'>
+                <Button color="danger" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
-                <Button color="danger" className='my-0.5'>
+                <Button color="danger" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
               </div>
@@ -131,19 +154,19 @@ const Poems = () => {
               }
             >
               <div className=' flex flex-col mx-4'>
-                <Button color="success" className='my-0.5'>
+                <Button color="success" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
-                <Button color="success" className='my-0.5'>
+                <Button color="success" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
-                <Button color="success" className='my-0.5'>
+                <Button color="success" variant='light' className='my-0.5'>
                   Secondary
                 </Button>
               </div>
             </AccordionItem>
           </Accordion>
-          <div className="col-span-7 ">
+          <div className="col-span-7 h-[calc(100vh-4rem)] overflow-y-auto">
             {
               poemList.map(poemItem => (
                 <Poem
@@ -154,12 +177,46 @@ const Poems = () => {
                   content={poemItem.content}
                   tags={poemItem.tags}
                   followers={poemItem.followers}
-                  
+
                 />
               ))
             }
           </div>
-          <div className="col-span-3 bg-blue-300 p-4 mt-4"></div>
+          <div className="col-span-3 mt-1 mr-1 h-[calc(100vh-4rem)] overflow-y-auto">
+            <Card className="">
+              <CardHeader className="flex gap-3">
+
+
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 40 40">
+                  <path fill="#f78f8f" d="M20 31.441L8.5 37.191 8.5 2.5 31.5 2.5 31.5 37.191z"></path><path fill="#c74343" d="M31,3v33.382l-10.553-5.276L20,30.882l-0.447,0.224L9,36.382V3H31 M32,2H8v36l12-6l12,6V2L32,2z"></path>
+                </svg>
+
+
+                <div className="flex flex-col">
+                  <p className="text-md font-bold text-red-500">Popular ths week</p>
+
+                </div>
+              </CardHeader>
+              <Divider />
+              <CardBody>
+              {
+              poemList.map(poemItem => (
+                <Poem
+                  key={poemItem.id}
+                  user={poemItem.user}
+                  duration={poemItem.duration}
+                  title={poemItem.title}
+                  content={poemItem.content}
+                  tags={poemItem.tags}
+                  followers={poemItem.followers}
+
+                />
+              ))
+            }
+              </CardBody>
+              <Divider />
+            </Card>
+          </div>
         </div>
       </div>
 
