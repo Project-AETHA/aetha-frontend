a5fd01bac6ebfc69b86007c683415ba6883e08fa
NightHawkNB <nipunbathiya1256@outlook.com>
2024-07-18 07:55:37 +0530

    Sidebar optimization - Completed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,10 @@
 import React, { useState } from "react";
 import { HiMenuAlt3 } from "react-icons/hi";
-import { MdAttachMoney, MdOutlineDashboard } from "react-icons/md";
-import { LuPenTool } from "react-icons/lu";
-import { RiAdvertisementLine } from "react-icons/ri";
-import { LiaBookSolid } from "react-icons/lia";
 import { Link } from "react-router-dom";
 import {Accordion, AccordionItem} from "@nextui-org/react";
 import "./sidebar_styles.css";
 
-export default function AuthorSidebar () {
-
-    const menus = [
-        { name: "Overview", link: "/author", icon: MdOutlineDashboard },
-        { name: "Publishes", link: "/author/publishes", icon: LiaBookSolid,
-            subMenus: [
-                { name: "Poems", link: "/author/publishes/poems", icon: LiaBookSolid },
-                { name: "Short Stories", link: "/author/publishes/short-stories", icon: LiaBookSolid },
-                { name: "Novels", link: "/author/publishes/novels", icon: LiaBookSolid },
-                { name: "Nisades", link: "/author/publishes/nisades", icon: LiaBookSolid },
-            ]
-        },
-        { name: "Notes", link: "/author/notes", icon: LuPenTool },
-        { name: "Advertising", link: "/author/advertising", icon: RiAdvertisementLine },
-        { name: "E-Book Selling", link: "/author/ebooks", icon: MdAttachMoney },
-        { name: "Create Poem", link: "/author/publishes/poem/create", icon: MdAttachMoney, margin: true },
-    ];
+export default function AuthorSidebar ({menus}) {
 
     const [open, setOpen] = useState(true);
 
@@ -89,29 +69,6 @@ export default function AuthorSidebar () {
                                     ))}
                                 </AccordionItem>
                             </Accordion>
-                            // <Link
-                            //     to={menu?.link}
-                            //     key={index}
-                            //     className={` ${
-                            //         menu?.margin && "mt-5"
-                            //     } text-foreground-900 hover:text-foreground-50 dark:hover:text-foreground-900 group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
-                            // >
-                            //     <div>{React.createElement(menu?.icon, {size: "20"})}</div>
-                            //     <h2
-                            //         className={`whitespace-pre duration-500 ${
-                            //             !open && "opacity-0 translate-x-28 overflow-hidden"
-                            //         }`}
-                            //     >
-                            //         {menu?.name}
-                            //     </h2>
-                            //     <h2
-                            //         className={`${
-                            //             open && "hidden"
-                            //         } absolute z-50 left-48 bg-white font-semibold whitespace-pre text-black rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
-                            //     >
-                            //         {menu?.name}
-                            //     </h2>
-                            // </Link>
                         )
                     }
                 })}
