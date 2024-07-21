fde246483a95c40836bee4877ef91d3f8c4e7e13
NightHawkNB <nipunbathiya1256@outlook.com>
2024-07-21 10:09:45 +0530

    Image uploader

--- src/components/common/ImageUpload.jsx
+++ src/components/common/ImageUpload.jsx
@@ -1,15 +1,16 @@
-import { Image, Input, Button } from '@nextui-org/react';
+import { Image, Button } from '@nextui-org/react';
 import { useState } from 'react';
+import { FileUploader } from "react-drag-drop-files";
 
 export default function ImageUpload({ files, setFiles }) {
     const [imagePreview, setImagePreview] = useState(null);
 
-    function handleUploadClick(e) {
-        const selectedFiles = e.target.files;
-        if (selectedFiles.length > 0) {
-            setFiles(selectedFiles); // Handle multiple files if needed
-            setImagePreview(URL.createObjectURL(selectedFiles[0]));
-        }
+    // Allowed file types
+    const fileTypes = ["JPEG", "PNG", "GIF"];
+
+    function handleUploadClick(files) {
+        setFiles(files); // Handle multiple files if needed
+        setImagePreview(URL.createObjectURL(files));
     }
 
     function handleClearClick() {
@@ -21,27 +22,39 @@ export default function ImageUpload({ files, setFiles }) {
 
     return (
         <div className="flex flex-col items-center justify-evenly flex-wrap gap-4">
-            <Image
-                width={200}
-                height={160}
-                alt="Image preview"
-                src={imagePreview || "http://localhost:8080/images/complaints/669aa497a1e4c96348da9c51_1.png"}
-            />
+            {imagePreview &&
+                (<Image
+                    width={200}
+                    height={160}
+                    alt="Image preview"
+                    src={imagePreview || "http://localhost:8080/images/complaints/669aa497a1e4c96348da9c51_1.png"}
+                />)
+            }
 
             <div className="flex gap-2">
-                <label>
-                    <div className="bg-purple-100 text-purple-700 text-medium py-2 px-4 rounded-xl hover:cursor-pointer active:scale-95">
-                        Select Image
-                    </div>
-                    <input
-                        type="file"
-                        name="files"
-                        className="hidden"
-                        id="complaint_image_upload"
-                        onChange={handleUploadClick}
-                    />
+                {/*<label>*/}
+                {/*    <div className="bg-purple-100 text-purple-700 text-medium py-2 px-4 rounded-xl hover:cursor-pointer active:scale-95">*/}
+                {/*        Select Image*/}
+                {/*    </div>*/}
+                {/*    <input*/}
+                {/*        type="file"*/}
+                {/*        name="files"*/}
+                {/*        className="hidden"*/}
+                {/*        id="complaint_image_upload"*/}
+                {/*        onChange={handleUploadClick}*/}
+                {/*    />*/}
 
-                </label>
+                {/*</label>*/}
+
+                {!imagePreview &&
+                    (<FileUploader
+                        multiple={false}
+                        handleChange={handleUploadClick}
+                        name="files"
+                        types={fileTypes}
+                    />)
+                }
+
                 {imagePreview && (
                     <Button
                         variant="flat"
