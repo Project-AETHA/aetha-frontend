7bbb976e92f5ef82030aadc4ae641ad2ed4162d3
Nipun Bathiya <2021cs067@stu.ucsc.cmb.ac.lk>
2024-07-25 10:51:21 +0530

    Update DefaultLayout.jsx

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer.jsx";
 export default function DefaultLayout (props) {
     return (
         <>
-            <div className="alt-container px-2 !min-w-full !w-full">
+            <div className="alt-container">
                 {props.children}
             </div>
             <Footer/>
