0cc2746a3a9d918cc9050cc5fbfbea0d97ad272e
hansimadhusha <madhushahansini.perera@gmail.com>
2024-07-25 08:48:12 +0530

    content management page

--- src/components/routes/Admin.jsx
+++ src/components/routes/Admin.jsx
@@ -1,11 +1,13 @@
 import UserManagement from "../../pages/Admin/UserManagement/UserManagement.jsx";
 import SingleUser from "../../pages/Admin/UserManagement/SingleUser.jsx";
+import ContentManagemenet from "../../pages/Admin/UserManagement/ContentManagemenet.jsx";
 
 
 export default function RoutesAdmin () {
     return [
         { path: '/admin', element: <div className="bg-blue-200">Custom Content</div>, layout: "admin_dashboard" },
         { path: '/admin/users', element: <UserManagement />, layout: "admin_dashboard" },
-        { path: '/admin/users/single', element: <SingleUser />, layout: "admin_dashboard" },
+        { path: '/admin/users/:userId', element: <SingleUser />, layout: "admin_dashboard" },
+        { path: '/admin/content', element: <ContentManagemenet/>, layout: "admin_dashboard" },
     ]
 }
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer.jsx";
 export default function DefaultLayout (props) {
     return (
         <>
-            <div className="alt-container">
+            <div className="alt-container px-2 !min-w-full !w-full">
                 {props.children}
             </div>
             <Footer/>
