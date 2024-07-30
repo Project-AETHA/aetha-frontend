b3c7d103bda13c97b32d69a23f5e68e62a6cd3c2
NightHawkNB <nipunbathiya1256@outlook.com>
2024-07-30 21:48:28 +0530

    Personal complaints - in progress

--- src/pages/Support/ComplaintDetails.jsx
+++ src/pages/Support/ComplaintDetails.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Spinner, Input, Chip, Image } from "@nextui-org/react";
+import { HiDocument } from "react-icons/hi2";
 
 export default function ComplaintDetails() {
   const [loading, setLoading] = useState(false);
@@ -39,46 +40,51 @@ export default function ComplaintDetails() {
     <div className="flex flex-col items-center justify-center min-h-[calc(80dvh-70px)] bg-white mx-2">
       {!loading && (
         <div className="flex flex-col gap-4 !text-black">
+          <h1 className="font-semibold text-large">Complaint Details</h1>
 
-          <h1 className="font-semibold text-large mx-auto">Complaint Details</h1>
-
-          <div className="flex gap-8 items-center">
-            <Input
-              key="title"
-              type="text"
-              label="Title"
-              disabled
-              size="md"
-              labelPlacement="outside"
-              className="min-w-[200px] max-w-[400px] lg:min-w-[400px] rounded-xl"
-              value={title}
-            />
-            <Chip
-              color="warning"
-              size="lg"
-              className="capitalize"
-              variant="flat"
-            >
-              {category}
-            </Chip>
-          </div>
-          
-          <Input
-              key="description"
-              type="text"
-              label="Description"
-              disabled
-              size="md"
-              labelPlacement="outside"
-              className="min-w-[200px] max-w-[400px] lg:min-w-[400px] rounded-xl"
-              value={description}
-            />
-
-            <p className="mt-6">Attachements</p>
-            <div className="flex gap-2">
-                <Image src={`http://localhost:8080${image}`} width={300} />
-                <Image src={`http://localhost:8080${image}`} width={300} />
+          <div className="flex items-center gap-6">
+            <div className="flex flex-col items-center grow gap-4">
+              <Input
+                key="title"
+                type="text"
+                label="Title"
+                disabled
+                size="md"
+                labelPlacement="outside"
+                className="min-w-[200px] max-w-[400px] lg:min-w-[400px] rounded-xl"
+                value={title}
+              />
+              <Input
+                key="description"
+                type="text"
+                label="Description"
+                disabled
+                size="md"
+                labelPlacement="outside"
+                className="min-w-[200px] max-w-[400px] lg:min-w-[400px] rounded-xl"
+                value={description}
+              />
             </div>
+            <div>
+              <Chip
+                color="warning"
+                size="lg"
+                className="capitalize col-span-2"
+                variant="flat"
+              >
+                <div className="flex gap-2">
+                  <HiDocument size={20} />
+                  <p>{category}</p>
+                </div>
+              </Chip>
+            </div>
+          </div>
+
+          <p className="mt-6">Attachements</p>
+          <div className="flex gap-2 flex-wrap sm:flex-nowrap">
+            <Image src={`http://localhost:8080${image}`} width={300} />
+            <Image src={`http://localhost:8080${image}`} width={300} />
+          </div>
         </div>
       )}
 
