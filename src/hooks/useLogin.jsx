d4db8f536cf7026ff44a89f99ad77b21d14ac0a1
hansimadhusha <madhushahansini.perera@gmail.com>
2024-07-30 14:48:10 +0530

    admin stats

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -35,7 +35,11 @@ export const useLogin = () => {
         dispatch({ type: "LOGIN", payload: {user: response.data.content.user, token: response.data.content.jwtToken} })
         setLoading(false)
 
-        navigate("/")
+        if(response.data.content.roles.includes("ROLE_ADMIN")){
+          navigate("/admin")
+        }
+        else{navigate("/")}
+        
       }
     }
   }
