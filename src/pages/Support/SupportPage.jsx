bbce054e9a1bf86a1571f279da57ed5bef6a7122
NightHawkNB <nipunbathiya1256@outlook.com>
2024-07-31 20:43:00 +0530

    Admin/Complaints in progress

--- src/pages/Support/SupportPage.jsx
+++ src/pages/Support/SupportPage.jsx
@@ -12,147 +12,147 @@ import { MdError } from "react-icons/md";
 import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 
 const SupportPage = () => {
-  const [title, setTitle] = useState("");
-  const [errorTitle, setErrorTitle] = useState(false);
-  const [category, setCategory] = useState("");
-  const [errorCategory, setErrorCategory] = useState(false);
-  const [description, setDescription] = useState("");
-  const [errorDescription, setErrorDescription] = useState(false);
-  const [files, setFiles] = useState(null);
+    const [title, setTitle] = useState("");
+    const [errorTitle, setErrorTitle] = useState(false);
+    const [category, setCategory] = useState("");
+    const [errorCategory, setErrorCategory] = useState(false);
+    const [description, setDescription] = useState("");
+    const [errorDescription, setErrorDescription] = useState(false);
+    const [files, setFiles] = useState(null);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const { toast } = useToast();
+    const { toast } = useToast();
 
-  const createTicket = async (e) => {
-    e.preventDefault();
+    const createTicket = async (e) => {
+        e.preventDefault();
 
-    if (title === "") setErrorTitle(true);
-    else setErrorTitle(false);
-    if (category === "") setErrorCategory(true);
-    else setErrorCategory(false);
-    if (description === "") setErrorDescription(true);
-    else setErrorDescription(false);
+        if (title === "") setErrorTitle(true);
+        else setErrorTitle(false);
+        if (category === "") setErrorCategory(true);
+        else setErrorCategory(false);
+        if (description === "") setErrorDescription(true);
+        else setErrorDescription(false);
 
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("category", category);
-    formData.append("description", description);
+        const formData = new FormData();
+        formData.append("title", title);
+        formData.append("category", category);
+        formData.append("description", description);
 
-    // Append each file in the files array
-    if (files) {
-      console.log(files);
-      formData.append("files", files);
-    }
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/api/support/create_ticket",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
+        // Append each file in the files array
+        if (files) {
+            console.log(files);
+            formData.append("files", files);
         }
-      );
 
-      console.log(response);
+        try {
+            const response = await axios.post(
+                "http://localhost:8080/api/support/create_ticket",
+                formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
 
-      if (response.data.code === "00") {
-        // Code for creating a custom title for the popup alert
+            console.log(response);
 
-        let title = (
-          <div className="flex gap-2 items-center text-green-600">
-            <IoCheckmarkDoneCircleSharp size={20} />
-            <p className="text-green-700">Success</p>
-          </div>
-        );
+            if (response.data.code === "00") {
+                // Code for creating a custom title for the popup alert
 
-        createAlert(
-          title,
-          "Ticket created successfully",
-          response.data.content.id,
-          "success"
-        );
-        clearAll();
-      }
-    } catch (error) {
-      // Code for creating a custom title for the popup alert
+                let title = (
+                    <div className="flex gap-2 items-center text-green-600">
+                        <IoCheckmarkDoneCircleSharp size={20} />
+                        <p className="text-green-700">Success</p>
+                    </div>
+                );
 
-      let title = (
-        <div className="flex gap-2 items-center text-red-800">
-          <MdError size={20} />
-          <p className="text-red-700">Error !</p>
-        </div>
-      );
+                createAlert(
+                    title,
+                    "Ticket created successfully",
+                    response.data.content.id,
+                    "success"
+                );
+                clearAll();
+            }
+        } catch (error) {
+            // Code for creating a custom title for the popup alert
 
-      createAlert(title, "Ticket creation failed", "#ERROR", "danger");
-      console.error("Error creating ticket:", error);
+            let title = (
+                <div className="flex gap-2 items-center text-red-800">
+                    <MdError size={20} />
+                    <p className="text-red-700">Error !</p>
+                </div>
+            );
+
+            createAlert(title, "Ticket creation failed", "#ERROR", "danger");
+            console.error("Error creating ticket:", error);
+        }
+    };
+
+    const clearAll = () => {
+        setTitle("");
+        setCategory("");
+        setDescription("");
+        setFiles(null);
+    };
+
+    function createAlert(title, description, complaintId, bcolor = "secondary") {
+        toast({
+            title: title,
+            description: description,
+            action: (
+                <Button
+                    variant="flat"
+                    radius="sm"
+                    className="!py-1"
+                    color={bcolor}
+                    altText="View"
+                    onClick={() => {
+                        navigate(`/support/${complaintId}`);
+                    }}
+                >
+                    View
+                </Button>
+            ),
+        });
     }
-  };
 
-  const clearAll = () => {
-    setTitle("");
-    setCategory("");
-    setDescription("");
-    setFiles(null);
-  };
-
-  function createAlert(title, description, complaintId, bcolor = "secondary") {
-    toast({
-      title: title,
-      description: description,
-      action: (
-        <Button
-          variant="flat"
-          radius="sm"
-          className="!py-1"
-          color={bcolor}
-          altText="View"
-          onClick={() => {
-            navigate(`/support/${complaintId}`);
-          }}
-        >
-          View
-        </Button>
-      ),
-    });
-  }
-
-  return (
-    <div className="alt-container">
-      <div className="p-2">
-        <h2 className="font-bold text-black dark:text-white mb-2">
-          Suggest to create a support ticket
-        </h2>
-        <div className="bg-foreground-50 text-black rounded-lg px-2 py-6 flex justify-center">
-          <CreateTicket
-            createTicket={createTicket}
-            setTitle={setTitle}
-            setCategory={setCategory}
-            setDescription={setDescription}
-            setFiles={setFiles}
-            title={title}
-            category={category}
-            description={description}
-            files={files}
-            clearAll={clearAll}
-            errorCategory={errorCategory}
-            errorDescription={errorDescription}
-            errorTitle={errorTitle}
-          />
+    return (
+        <div className="alt-container">
+            <div className="p-2">
+                <h2 className="font-bold text-black dark:text-white mb-2">
+                    Suggest to create a support ticket
+                </h2>
+                <div className="bg-foreground-50 text-black rounded-lg px-2 py-6 flex justify-center">
+                    <CreateTicket
+                        createTicket={createTicket}
+                        setTitle={setTitle}
+                        setCategory={setCategory}
+                        setDescription={setDescription}
+                        setFiles={setFiles}
+                        title={title}
+                        category={category}
+                        description={description}
+                        files={files}
+                        clearAll={clearAll}
+                        errorCategory={errorCategory}
+                        errorDescription={errorDescription}
+                        errorTitle={errorTitle}
+                    />
+                </div>
+            </div>
+            <div className="">
+                <div className="text-black dark:text-white text-lg font-bold mb-2">
+                    Active Tickets
+                </div>
+                <div>
+                    <SupportTable createTicket={createTicket} />
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="">
-        <div className="text-black dark:text-white text-lg font-bold mb-2">
-          Active Tickets
-        </div>
-        <div>
-          <SupportTable createTicket={createTicket} />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SupportPage;
--- src/pages/Support/components/SupportTable.jsx
+++ src/pages/Support/components/SupportTable.jsx
@@ -1,12 +1,12 @@
 import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  Chip,
-  Spinner
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+    Chip,
+    Spinner
 } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -16,205 +16,205 @@ import { FaRegEdit } from "react-icons/fa";
 import {MdDeleteForever, MdError, MdOutlineRefresh} from "react-icons/md";
 import {IoCheckmarkDoneCircleSharp} from "react-icons/io5";
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { useToast } from "@/components/ui/use-toast";
 
 const formatDate = (dateString, default_style = true) => {
-  // Parse the given date string
-  const date = parseISO(dateString);
-  // Format the date to the desired format
-  return default_style ? format(date, "yyyy MMM dd") : format(date, "do MMM");
+    // Parse the given date string
+    const date = parseISO(dateString);
+    // Format the date to the desired format
+    return default_style ? format(date, "yyyy MMM dd") : format(date, "do MMM");
 };
 
 function SupportTable({ createTicket }) {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const { toast } = useToast();
+    const { toast } = useToast();
 
-  const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const [tickets, setTickets] = useState([]);
+    const [tickets, setTickets] = useState([]);
 
-  const getData = async () => {
-    setIsLoading(true);
+    const getData = async () => {
+        setIsLoading(true);
 
-    try {
-      const response = await axios.get(
-        "http://localhost:8080/api/support/get_my_tickets",
-        {
-          timeout: 3000, // Set timeout to 3 seconds
+        try {
+            const response = await axios.get(
+                "http://localhost:8080/api/support/get_my_tickets",
+                {
+                    timeout: 3000, // Set timeout to 3 seconds
+                }
+            );
+
+            if (response.status === 200) {
+                if (response.data.code === "00") {
+                    setTickets(response.data.content);
+                } else {
+                    console.log("Error reported from server");
+                }
+            } else {
+                console.log("Error while fetching data");
+            }
+        } catch (error) {
+            if (error.code === "ECONNABORTED") {
+                console.log("Request timed out");
+            } else {
+                console.log("Error while fetching data:", error.message);
+            }
         }
-      );
 
-      if (response.status === 200) {
-        if (response.data.code === "00") {
-          setTickets(response.data.content);
-        } else {
-          console.log("Error reported from server");
-        }
-      } else {
-        console.log("Error while fetching data");
-      }
-    } catch (error) {
-      if (error.code === "ECONNABORTED") {
-        console.log("Request timed out");
-      } else {
-        console.log("Error while fetching data:", error.message);
-      }
+        setIsLoading(false);
+    };
+
+    function createAlert(title, description) {
+        toast({
+            title: title,
+            description: description,
+        });
     }
 
-    setIsLoading(false);
-  };
+    const deleteTicket = async (complaintId) => {
 
-  function createAlert(title, description) {
-    toast({
-      title: title,
-      description: description,
-    });
-  }
+        try {
+            const response = await axios.delete("http://localhost:8080/api/support/delete_ticket/" + complaintId);
 
-  const deleteTicket = async (complaintId) => {
+            if (response.data.code === "00") {
+                // Code for creating a custom title for the popup alert
 
-    try {
-      const response = await axios.delete("http://localhost:8080/api/support/delete_ticket/" + complaintId);
+                let title = (
+                    <div className="flex gap-2 items-center text-green-600">
+                        <IoCheckmarkDoneCircleSharp size={20} />
+                        <p className="text-green-700">Success</p>
+                    </div>
+                );
 
-      if (response.data.code === "00") {
-        // Code for creating a custom title for the popup alert
+                createAlert(
+                    title,
+                    "Ticket deleted successfully",
+                    "success"
+                );
 
-        let title = (
-            <div className="flex gap-2 items-center text-green-600">
-              <IoCheckmarkDoneCircleSharp size={20} />
-              <p className="text-green-700">Success</p>
-            </div>
-        );
+                navigate("/support")
+            }
+        } catch (error) {
+            // Code for creating a custom title for the popup alert
 
-        createAlert(
-            title,
-            "Ticket deleted successfully",
-            "success"
-        );
+            let title = (
+                <div className="flex gap-2 items-center text-red-800">
+                    <MdError size={20} />
+                    <p className="text-red-700">Error !</p>
+                </div>
+            );
 
-        navigate("/support")
-      }
-    } catch (error) {
-      // Code for creating a custom title for the popup alert
+            createAlert(title, "Ticket deletion failed", "danger");
+            console.error("Error deleting the ticket:", error);
+        }
+    };
 
-      let title = (
-          <div className="flex gap-2 items-center text-red-800">
-            <MdError size={20} />
-            <p className="text-red-700">Error !</p>
-          </div>
-      );
-
-      createAlert(title, "Ticket deletion failed", "danger");
-      console.error("Error deleting the ticket:", error);
+    function handleRetry() {
+        getData();
     }
-  };
 
-  function handleRetry() {
-    getData();
-  }
+    useEffect(() => {
+        getData();
+    }, [createTicket]);
 
-  useEffect(() => {
-    getData();
-  }, [createTicket]);
-
-  return (
-    <Table
-      aria-label="Support Tickets Table"
-      className="text-black dark:text-white"
-    >
-      <TableHeader>
-        <TableColumn>TITLE</TableColumn>
-        <TableColumn>CREATED AT</TableColumn>
-        <TableColumn>STATUS</TableColumn>
-        <TableColumn>ACTIONS</TableColumn>
-      </TableHeader>
-      <TableBody
-        emptyContent={
-          <div
-            className="flex gap-2 items-center justify-center"
-            onClick={handleRetry}
-          >
-            <p>No rows to display.</p>
-            <span className="text-primary border-b-2 border-primary hover:cursor-pointer flex gap-1 items-center">
+    return (
+        <Table
+            aria-label="Support Tickets Table"
+            className="text-black dark:text-white"
+        >
+            <TableHeader>
+                <TableColumn>TITLE</TableColumn>
+                <TableColumn>CREATED AT</TableColumn>
+                <TableColumn>STATUS</TableColumn>
+                <TableColumn>ACTIONS</TableColumn>
+            </TableHeader>
+            <TableBody
+                emptyContent={
+                    <div
+                        className="flex gap-2 items-center justify-center"
+                        onClick={handleRetry}
+                    >
+                        <p>No rows to display.</p>
+                        <span className="text-primary border-b-2 border-primary hover:cursor-pointer flex gap-1 items-center">
               Retry <MdOutlineRefresh />
             </span>
-          </div>
-        }
-        items={tickets}
-        isLoading={isLoading}
-        loadingContent={<Spinner />}
-      >
-        {(item) => (
-          <TableRow
-            key={item.id}
-            className="hover:cursor-pointer"
-          >
-            <TableCell className="truncate text-pretty">{item.title}</TableCell>
-            <TableCell className="text-nowrap">
+                    </div>
+                }
+                items={tickets}
+                isLoading={isLoading}
+                loadingContent={<Spinner />}
+            >
+                {(item) => (
+                    <TableRow
+                        key={item.id}
+                        className="hover:cursor-pointer"
+                    >
+                        <TableCell className="truncate text-pretty">{item.title}</TableCell>
+                        <TableCell className="text-nowrap">
               <span className="hidden sm:block">
                 {formatDate(item.createdAt)}
               </span>
-              <span className="block sm:hidden">
+                            <span className="block sm:hidden">
                 {formatDate(item.createdAt, false)}
               </span>
-            </TableCell>
-            <TableCell>
-              {item.status === "PENDING" ? (
-                <Chip radius="sm" variant="flat" size="sm" color="primary">
-                  Pending
-                </Chip>
-              ) : item.status === "SOLVED" ? (
-                <Chip radius="sm" variant="flat" size="sm" color="success">
-                  Solved
-                </Chip>
-              ) : (
-                <Chip radius="sm" variant="flat" size="sm" color="danger">
-                  Cancelled
-                </Chip>
-              )}
-            </TableCell>
-            <TableCell className="flex gap-2">
-              <Chip variant="flat" radius="sm" className="px-2" color="primary" onClick={() => navigate("/support/" + item.id)}>
-                <FaRegEdit size={13} />
-              </Chip>
-              <AlertDialog>
-                <AlertDialogTrigger asChild>
-                  <Chip variant="flat" radius="sm" className="px-2" color="danger">
-                    <MdDeleteForever size={15} />
-                  </Chip>
+                        </TableCell>
+                        <TableCell>
+                            {item.status === "PENDING" ? (
+                                <Chip radius="sm" variant="flat" size="sm" color="primary">
+                                    Pending
+                                </Chip>
+                            ) : item.status === "SOLVED" ? (
+                                <Chip radius="sm" variant="flat" size="sm" color="success">
+                                    Solved
+                                </Chip>
+                            ) : (
+                                <Chip radius="sm" variant="flat" size="sm" color="danger">
+                                    Cancelled
+                                </Chip>
+                            )}
+                        </TableCell>
+                        <TableCell className="flex gap-2">
+                            <Chip variant="flat" radius="sm" className="px-2" color="primary" onClick={() => navigate("/support/" + item.id)}>
+                                <FaRegEdit size={13} />
+                            </Chip>
+                            <AlertDialog>
+                                <AlertDialogTrigger asChild>
+                                    <Chip variant="flat" radius="sm" className="px-2" color="danger">
+                                        <MdDeleteForever size={15} />
+                                    </Chip>
 
-                  {/* This element is the alert confirmation window from shadcn/ui */}
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                      This action cannot be undone. This will permanently delete this complaint data from our servers.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction className="bg-danger/75 hover:bg-danger" onClick={() => deleteTicket(item.id)}>Continue</AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-  );
+                                    {/* This element is the alert confirmation window from shadcn/ui */}
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                            This action cannot be undone. This will permanently delete this complaint data from our servers.
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                        <AlertDialogAction className="bg-danger/75 hover:bg-danger" onClick={() => deleteTicket(item.id)}>Continue</AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </TableCell>
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
+    );
 }
 
 export default SupportTable;
