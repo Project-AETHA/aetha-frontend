2e778222a490b078165620fd734c2228bb772992
NightHawkNB <nipunbathiya1256@outlook.com>
2024-11-10 14:20:34 +0530

    updated the NovelLandingPage with tanstack

--- src/pages/LandingPage/GeneralLandingPage.jsx
+++ src/pages/LandingPage/GeneralLandingPage.jsx
@@ -59,6 +59,8 @@ export default function GeneralLandingPage() {
                     style={{filter: "blur(8px)"}}
                 />
 
+                {!isLoading && console.log(data.slice(0, 4))}
+
                 <div
                     className="absolute inset-0 rounded p-4 min-w-[500px] z-10 bg-trasparent flex flex-col gap-52 justify-center"
                 >
